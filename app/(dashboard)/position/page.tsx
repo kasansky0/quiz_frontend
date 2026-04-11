08374cde4f49ee11ce96a017ff9f53be97ebd02e
Trojan NoEscape <trojannoescape@Trojans-MacBook-Air.local>
2026-04-11 17:46:20 -0400

    Fix job card touch behavior and UI polish

--- app/(dashboard)/position/page.tsx
+++ app/(dashboard)/position/page.tsx
@@ -99,11 +99,10 @@ export default function AdsPage() {
                     <Link
                         key={job.id}
                         href={`/position/apply/${job.id}`}
-                        className="block w-full cursor-pointer rounded-lg transition flex flex-col items-center justify-center gap-1 mb-4
-                                hover:bg-zinc-900 active:scale-[0.98]
-                                active:bg-transparent focus:bg-transparent
-                                [-webkit-tap-highlight-color:transparent]
-                                select-none touch-manipulation"
+                        className="block w-full rounded-lg transition
+                            flex flex-col items-center justify-center gap-1 mb-4
+                            active:bg-transparent focus:bg-transparent
+                            [-webkit-tap-highlight-color:transparent]"
                     >
                         <div className="text-sm font-semibold">
                             Company: {job.company}
