bbddda47c3b34e9a6ade9695ed7fd3a481258ecf
Trojan NoEscape <trojannoescape@Trojans-MacBook-Air.local>
2026-07-06 12:27:37 -0400

    move post view to /post/[id] page with comments 3

--- app/(dashboard)/feed/ChatStats.tsx
+++ app/(dashboard)/feed/ChatStats.tsx
@@ -610,7 +610,7 @@ export default function ChatStats() {
                                         return (
                                             <div
                                                 key={post.id}
-                                                onClick={() => router.push(`/post/${post.id}`)}
+                                                onClick={() => handleActivatePost(post)}
                                                 className={`
                                                 relative cursor-pointer
                                                 rounded-2xl p-3 w-full
