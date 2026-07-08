708027e20ab993300a355e1a4979727b9184c18e
Trojan NoEscape <trojannoescape@Trojans-MacBook-Air.local>
2026-07-08 16:48:57 -0400

    Add single comment SWR fetch support 12

--- app/(dashboard)/sidebar/UserStats.tsx
+++ app/(dashboard)/sidebar/UserStats.tsx
@@ -319,13 +319,7 @@ export default function UserStats({ nickname, platformStats, loading, onLinkClic
 
                         {/* Nickname text */}
                         <span className="lg:group-hover:text-blue-400 flex items-center">
-                            {loading ? (
-                                <span className="flex items-center justify-center w-6 h-6">
-                                    <StatLoaderIcon />
-                                </span>
-                            ) : (
-                                nickname ?? "User"
-                            )}
+                            {nickname ?? "User"}
                         </span>
                     </div>
                 </div>
