0f7e67712ba8f0885c5829f3acca57354e70146f
Trojan NoEscape <trojannoescape@Trojans-MacBook-Air.local>
2026-08-23 15:13:15 -0400

    Initialize Capacitor mobile app

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.netaprep.app',
+  appName: 'netaprep',
+  webDir: 'public'
+};
+
+export default config;
